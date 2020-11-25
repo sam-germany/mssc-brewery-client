8c38aab3d97b708f30f7af42ea5f1ba1a0f2c795
sunny <sunny.keila24@gmail.com>
2020-11-25 17:56:02 +0100

    updated

--- src/main/java/com/springframeworkguru/msscbreweryclient/web/client/BreweryClient.java
+++ src/main/java/com/springframeworkguru/msscbreweryclient/web/client/BreweryClient.java
@@ -2,11 +2,13 @@ package com.springframeworkguru.msscbreweryclient.web.client;
 
 
 import com.springframeworkguru.msscbreweryclient.web.model.BeerDto;
+import com.springframeworkguru.msscbreweryclient.web.model.CustomerDto;
 import org.springframework.boot.context.properties.ConfigurationProperties;
 import org.springframework.boot.web.client.RestTemplateBuilder;
 import org.springframework.stereotype.Component;
 import org.springframework.web.client.RestTemplate;
 
+import java.net.URI;
 import java.util.UUID;
 
 @Component
@@ -14,6 +16,7 @@ import java.util.UUID;
 public class BreweryClient {
 
     public final String BEER_PATH_V1 = "/api/v1/beer/";
+    public final String CUSTOMER_PATH_V1 = "/api/v1/customer/";
 
     private String apihost;
     private final RestTemplate restTemplate;
@@ -26,19 +29,50 @@ public class BreweryClient {
         return restTemplate.getForObject(apihost + BEER_PATH_V1 + uuid.toString(), BeerDto.class);
     }
 
+    public URI saveNewBeer(BeerDto beerDto) {
+        return restTemplate.postForLocation(apihost + BEER_PATH_V1, beerDto);
+    }
 
-    public void setApihost(String apihost) {
+    public void updateBeer(UUID uuid, BeerDto beerDto) {
+        restTemplate.put(apihost + BEER_PATH_V1   + uuid.toString(), beerDto );
+    }
+
+    public void deleteBeer(UUID uuid) {
+        restTemplate.delete(apihost + BEER_PATH_V1 + uuid);
+    }
+
+    public void setApihost(String apihost){
         this.apihost = apihost;
     }
 
+    public CustomerDto getCustomerById(UUID uuid) {
+        return restTemplate.getForObject(apihost + CUSTOMER_PATH_V1 + uuid.toString(), CustomerDto.class);
+    }
+
+    public URI saveNewCustomer(CustomerDto customerDto) {
+        return restTemplate.postForLocation(apihost + CUSTOMER_PATH_V1, customerDto);
+    }
+
+    public void updateCustomer(UUID customerId, CustomerDto customerDto) {
+        restTemplate.put(apihost + CUSTOMER_PATH_V1  + customerId.toString(), customerDto);
+    }
+
+    public void deleteCustomer(UUID customerId) {
+        restTemplate.delete(apihost + CUSTOMER_PATH_V1  + customerId);
+    }
 }
 
 /*
-ignoreInvalidFields
+(1)
+sfg.brewery  <-- class level  reference defined in the .properties file
+apihost    <-- is defined in the class so we use in the .properties file
+sfg.brewery.apihost=http://localhost:9999       <-- easy to understand
 
-Flag to indicate that when binding to this object invalid fields should be ignored.
-Invalid means invalid according to the binder that is used, and usually this means
-fields of the wrong type (or that cannot be coerced into the correct type).
+(2)
+ignoreInvalidFields= false    <-- somehow i do not understand the use of the attribute, but now i got  the point
+as we are trying to fetch the object, if some invalid fields are coming with that object, then just ignore those
+fields.
+
 
 
  */
---
+++ src/test/java/com/springframeworkguru/msscbreweryclient/web/client/BreweryClientTest.java
@@ -0,0 +1,79 @@
+package com.springframeworkguru.msscbreweryclient.web.client;
+
+import com.springframeworkguru.msscbreweryclient.web.model.BeerDto;
+import com.springframeworkguru.msscbreweryclient.web.model.CustomerDto;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+
+import java.net.URI;
+import java.util.UUID;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+@SpringBootTest
+class BreweryClientTest {
+
+    @Autowired
+    BreweryClient client;
+
+    @Test
+    void getBeerById() {
+        BeerDto dto = client.getBeerById(UUID.randomUUID());
+        assertNotNull(dto);
+    }
+
+    @Test
+    void testSaveNewBeer() {
+        BeerDto beerDto = BeerDto.builder().beerName("New Beer").build();
+
+        URI uri = client.saveNewBeer(beerDto);
+        assertNotNull(uri);
+        System.out.println(uri.toString());
+    }
+
+    @Test
+    void testUpdateBeer() {
+        BeerDto beerDto = BeerDto.builder().beerName("updated Beer").build();
+
+        client.updateBeer(UUID.randomUUID(), beerDto);
+    }
+
+    @Test
+    void testDeleteBeer() {
+        client.deleteBeer(UUID.randomUUID());
+    }
+
+    @Test
+    void getCustomerById() {
+        CustomerDto dto = client.getCustomerById(UUID.randomUUID());
+        assertNotNull(dto);
+      }
+
+    @Test
+    void testSaveNewCustomer() {
+        CustomerDto customerDto = CustomerDto.builder().name("Joe").build();
+        URI uri  = client.saveNewCustomer(customerDto);
+        assertNotNull(uri);
+        System.out.println(uri.toString());
+    }
+
+    @Test
+    void testUpdateCustomer() {
+        CustomerDto customerDto = CustomerDto.builder().name("new Joi").build();
+        client.updateCustomer(UUID.randomUUID(), customerDto);
+    }
+
+    @Test
+    void testDeleteCustomer() {
+        client.deleteCustomer(UUID.randomUUID());
+    }
+
+
+
+
+
+
+
+
+}
